bce554bcfd4b230a1d5d979fa33125110472f484
gnoes-ios <park_juseong@naver.com>
2025-03-17 13:24:44 +0900

    feat: 제네릭 함수c 구현

--- two_week/Juseong/AdvancedChallenges/AdvancedChallenges/Problem3.swift
+++ two_week/Juseong/AdvancedChallenges/AdvancedChallenges/Problem3.swift
@@ -17,14 +17,26 @@ import Foundation
 //private let result = a(numbers)
 
 // MARK: 2. String 배열 홀수번째 요소 반환
+//private let strings = ["가", "나", "다", "라", "마"]
+//
+//private func b(_ strings: [String]) -> [String] {
+//    strings.indices.filter { $0 % 2 == 0 }.map { strings[$0] }
+//}
+//
+//private let result = b(strings)
+
+// MARK: 3. 제네릭 함수 구현
+private let numbers = [1, 2, 3, 4, 5]
 private let strings = ["가", "나", "다", "라", "마"]
 
-private func b(_ strings: [String]) -> [String] {
-    strings.indices.filter { $0 % 2 == 0 }.map { strings[$0] }
+private func c<T>(_ inputs: [T]) -> [T] {
+    inputs.indices.filter { $0 % 2 == 0 }.map { inputs[$0] }
 }
 
-private let result = b(strings)
+private let numberResult = c(numbers)
+private let stringResult = c(strings)
 
 func problem3Result() {
-    print(result)
+    print(numberResult)
+    print(stringResult)
 }
