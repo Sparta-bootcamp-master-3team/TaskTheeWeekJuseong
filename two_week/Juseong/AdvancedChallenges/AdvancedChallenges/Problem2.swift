f88cdd4f100403eafecf0f4f9519f8fb84f9f3ac
gnoes-ios <park_juseong@naver.com>
2025-03-17 11:52:34 +0900

    feat: 고차함수 체이닝 - filter, map

--- two_week/Juseong/AdvancedChallenges/AdvancedChallenges/Problem2.swift
+++ two_week/Juseong/AdvancedChallenges/AdvancedChallenges/Problem2.swift
@@ -9,7 +9,11 @@ import Foundation
 
 private let numbers = [1, 2, 3, 4, 5]
 
-private let result = numbers.map { String($0) }
+// map 변환
+//private let result = numbers.map { String($0) }
+
+// 고차함수 체이닝 - filter, map
+private let result = numbers.filter { $0 % 2 == 0 }.map { String($0) }
 
 func problem2Result() {
     print(result)
