e7dcc1ff03f717aacf606b1560ab1ae1cea87830
gnoes-ios <park_juseong@naver.com>
2025-03-17 12:38:27 +0900

    feat: myMap함수 구현

--- two_week/Juseong/AdvancedChallenges/AdvancedChallenges/Problem2.swift
+++ two_week/Juseong/AdvancedChallenges/AdvancedChallenges/Problem2.swift
@@ -7,13 +7,20 @@
 
 import Foundation
 
-private let numbers = [1, 2, 3, 4, 5]
+private let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// map 변환
+// 1. map 변환
 //private let result = numbers.map { String($0) }
 
-// 고차함수 체이닝 - filter, map
-private let result = numbers.filter { $0 % 2 == 0 }.map { String($0) }
+// 2. 고차함수 체이닝 - filter, map
+//private let result = numbers.filter { $0 % 2 == 0 }.map { String($0) }
+
+//3. myMap 함수 구현
+private func myMap(numbers: [Int], closure: ((Int) -> String)) -> [String] {
+    numbers.map(closure)
+}
+
+private let result = myMap(numbers: numbers) { String($0) }
 
 func problem2Result() {
     print(result)
