5d977de8ebf8dfc1de5ee7e0bc1379b6543889d6
gnoes-ios <park_juseong@naver.com>
2025-03-17 12:58:17 +0900

    feat: 함수b 구현

--- two_week/Juseong/AdvancedChallenges/AdvancedChallenges/Problem3.swift
+++ two_week/Juseong/AdvancedChallenges/AdvancedChallenges/Problem3.swift
@@ -8,13 +8,22 @@
 import Foundation
 
 // MARK: 1. Int 배열 홀수번째 요소 반환
-private let numbers = [1, 2, 3, 4, 5]
+//private let numbers = [1, 2, 3, 4, 5]
+//
+//private func a(_ numbers: [Int]) -> [Int] {
+//    numbers.indices.filter { $0 % 2 == 0 }.map { numbers[$0] }
+//}
+//
+//private let result = a(numbers)
 
-private func a(_ numbers: [Int]) -> [Int] {
-    numbers.indices.filter { $0 % 2 == 0 }.map { numbers[$0] }
+// MARK: 2. String 배열 홀수번째 요소 반환
+private let strings = ["가", "나", "다", "라", "마"]
+
+private func b(_ strings: [String]) -> [String] {
+    strings.indices.filter { $0 % 2 == 0 }.map { strings[$0] }
 }
 
-private let result = a(numbers)
+private let result = b(strings)
 
 func problem3Result() {
     print(result)
