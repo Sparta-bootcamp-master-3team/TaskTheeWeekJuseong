ac42a159581303687894364aac8968a7146b77a4
gnoes-ios <park_juseong@naver.com>
2025-03-17 16:58:34 +0900

    feat: HybridCar 클래스 정의 및 인스턴스 생성과 엔진 교체

--- two_week/Juseong/AdvancedChallenges/AdvancedChallenges/Problem4.swift
+++ two_week/Juseong/AdvancedChallenges/AdvancedChallenges/Problem4.swift
@@ -19,11 +19,17 @@ private class ElectricEngine: Engine {
     }
 }
 
+private class HydrogenEngine: Engine {
+    override var engineType: String {
+        return "수소"
+    }
+}
+
 private class Car {
     let brand: String
     let model: String
     let year: String
-    let engine: Engine
+    var engine: Engine
     
     init(brand: String, model: String, year: String, engine: Engine) {
         self.brand = brand
@@ -41,4 +47,22 @@ private class ElectricCar: Car {
     init(brand: String, model: String, year: String, engine: ElectricEngine) {
         super.init(brand: brand, model: model, year: year, engine: engine)
     }
+}
+
+private class HybridCar: Car {
+    init(brand: String, model: String, year: String, engine: HydrogenEngine) {
+        super.init(brand: brand, model: model, year: year, engine: engine)
+    }
+    
+    func switchEngine(to newEngine: Engine) {
+        self.engine = newEngine
+    }
+}
+
+private let hybridCar = HybridCar(brand: "현대", model: "제네시스", year: "25년", engine: HydrogenEngine())
+
+func problem4Result() {
+    print(hybridCar.engine.engineType)
+    hybridCar.switchEngine(to: ElectricEngine())
+    print(hybridCar.engine.engineType)
 }
